b35c53fb15b15a2993b4326e9ad0e09bf4cebd2d
Abraham Williams <4braham@gmail.com>
2016-07-12 16:57:45 -0500

    Disable caching of quark count

--- app/helpers/counter_helper.rb
+++ app/helpers/counter_helper.rb
@@ -1,7 +1,7 @@
 module CounterHelper
   def current_quark_count
-    Rails.cache.fetch(:quark_count) do
-      Quark.sum(:count)
-    end
+    # Rails.cache.fetch(:quark_count) do
+    Quark.sum(:count)
+    # end
   end
 end
