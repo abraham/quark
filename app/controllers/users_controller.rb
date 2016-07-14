3ccc8da60503926399803e22aca56274d6916d1b
Abraham Williams <4braham@gmail.com>
2016-07-14 14:56:45 -0500

    Add several additional greetings

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
 
     if @user.save
       sign_in @user
-      flash[:notice] = "Get counting #{@user.name}!"
+      flash[:notice] = greeting(@user.name)
       redirect_to root_url
     else
       @user.errors.full_messages.each do |message|
@@ -19,6 +19,10 @@ class UsersController < ApplicationController
   end
 
   private
+
+  def greeting(name)
+    ["Get counting #{name}!", "Hello #{name}!", "Hi #{name}!", "Welcome #{name}!"].sample
+  end
 
   def user_params
     params.require(:user).permit(:name)
