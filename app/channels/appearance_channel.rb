8702f0727e3575e90f0dc7a81b7ca105392416d7
Abraham Williams <4braham@gmail.com>
2016-07-15 16:05:26 -0500

    Only show 5 online names

--- app/channels/appearance_channel.rb
+++ app/channels/appearance_channel.rb
@@ -35,7 +35,7 @@ class AppearanceChannel < ApplicationCable::Channel
   def current_user_stats
     total_count = ActionCable.server.connections.count
     users_count = Redis.current.scard(:online_users)
-    user_ids = Redis.current.srandmember(:online_users, 10)
+    user_ids = Redis.current.srandmember(:online_users, 5)
     {
       total_online: total_count,
       anonymous_online: total_count - users_count,
