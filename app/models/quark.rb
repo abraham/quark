209e33a0526117146645101c806cbeb44435562c
Abraham Williams <4braham@gmail.com>
2016-07-15 16:42:18 -0500

    Restricting creating Quarks somewhat

--- app/models/quark.rb
+++ app/models/quark.rb
@@ -1,4 +1,14 @@
 class Quark < ApplicationRecord
   belongs_to :user
+
+  scope :since, ->(datetime) { where(['created_at > ?', datetime]) }
+
   validates :count, presence: true, numericality: { greater_than: 0 }
+  validate :not_excessive_counting
+
+  private
+
+  def not_excessive_counting
+    errors.add(:quark, 'added too frequently') if user.quarks.since(5.seconds.ago).count > 5
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :quarks
   validates :name, presence: true
 
   def appear
