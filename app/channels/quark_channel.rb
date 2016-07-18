bc6c35c6743e5722f1c50e97354df82cad2eb2d7
Abraham Williams <4braham@gmail.com>
2016-07-18 15:19:11 -0500

    Create error/success handlers for QuarkChannel

--- app/channels/quark_channel.rb
+++ app/channels/quark_channel.rb
@@ -9,13 +9,23 @@ class QuarkChannel < ApplicationCable::Channel
 
     quark = Quark.new(count: data['count'], user: current_user)
     if quark.save
-      QuarkChannel.broadcast_to(:global, quark: quark, action: :created, status: :ok)
+      success :global, :created, quark: quark, total_count: Quark.total_count
     else
-      QuarkChannel.broadcast_to(self, action: :created, status: :error, messages: quark.errors.full_messages)
+      error self, :created, messages: quark.errors.full_messages
     end
   end
 
   def total_count
-    QuarkChannel.broadcast_to(self, action: :total_count, status: :ok, count: Quark.total_count)
+    success :total_count, total_count: Quark.total_count
+  end
+
+  private
+
+  def success(channel, action, values)
+    QuarkChannel.broadcast_to(channel, values.merge(action: action, status: :ok))
+  end
+
+  def error(channel, action, messages)
+    QuarkChannel.broadcast_to(channel, action: action, status: :error, messages: messages)
   end
 end
